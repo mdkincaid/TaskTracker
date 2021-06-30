4b64d6773260e814d191e187655db1904919edc4
Matthew Kincaid <matthew@kincaid.dev>
2021-06-29 23:36:45 -0400

    chore: updating version numbers to match current version

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const About = () => {
 	return (
 		<div>
-			<h4>Version 1.0.0</h4>
+			<h4>Version 1.2.0</h4>
 			<Link to='/'>Go Back</Link>
 		</div>
 	);
