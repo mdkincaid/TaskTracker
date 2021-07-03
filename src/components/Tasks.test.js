76f71cf967082acd1e7043883d7e678881d38a11
Matthew Kincaid <matthew@kincaid.dev>
2021-07-03 18:25:47 -0400

    test: updated to match proper testing patterns with react-testing-library

--- src/components/Tasks.test.js
+++ src/components/Tasks.test.js
@@ -1,44 +1,204 @@
 // react
 import React from 'react';
+import { Router, Route } from 'react-router-dom';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
+import { createMemoryHistory } from 'history';
+import userEvent from '@testing-library/user-event';
 
 // project
 import Tasks from './Tasks';
 
-const tasks = [
-	{
-		text: 'Test Task',
-		date: '2021-06-01T20:30:00.000Z',
-		reminder: false,
-		id: 14,
-	},
-	{
-		text: 'Food Shopping',
-		date: '2021-06-17T01:30:00.000Z',
-		reminder: false,
-		id: 15,
-	},
-	{
-		text: 'Test',
-		date: '2021-06-10T04:00:00.000Z',
-		reminder: false,
-		id: 16,
-	},
-];
-
 describe('Tasks', () => {
 	it('should render', () => {
-		const result = render(<Tasks tasks={tasks} />);
+		// arrange / act
+		const tasks = [
+			{
+				text: 'Fake Task #1',
+				date: '2021-06-01T20:30:00.000Z',
+				reminder: false,
+				id: 1,
+			},
+			{
+				text: 'Fake Task #2',
+				date: '2021-06-17T01:30:00.000Z',
+				reminder: false,
+				id: 2,
+			},
+		];
+		const history = createMemoryHistory();
+		const route = '/';
+		history.push(route);
+		const result = render(
+			<Router history={history}>
+				<Route>
+					<Tasks tasks={tasks} />
+				</Route>
+			</Router>
+		);
 
 		// assert
 		expect(result.container).toMatchSnapshot();
 	});
 
 	it('should render 3 tasks when 3 tasks are supplied', () => {
-		const result = render(<Tasks tasks={tasks} />);
+		// arrange / act
+		const tasks = [
+			{
+				text: 'Fake Task #1',
+				date: '2021-06-01T20:30:00.000Z',
+				reminder: false,
+				id: 1,
+			},
+			{
+				text: 'Fake Task #2',
+				date: '2021-06-17T01:30:00.000Z',
+				reminder: false,
+				id: 2,
+			},
+		];
+		const history = createMemoryHistory();
+		const route = '/';
+		history.push(route);
+		const result = render(
+			<Router history={history}>
+				<Route>
+					<Tasks tasks={tasks} />
+				</Route>
+			</Router>
+		);
+
+		// assert
+		expect(result.container.getElementsByClassName('task').length).toBe(2);
+	});
+
+	it('should render a 4th task when one is added', () => {
+		// arrange / act
+		const tasks = [
+			{
+				text: 'Fake Task #1',
+				date: '2021-06-01T20:30:00.000Z',
+				reminder: false,
+				id: 1,
+			},
+			{
+				text: 'Fake Task #2',
+				date: '2021-06-17T01:30:00.000Z',
+				reminder: false,
+				id: 2,
+			},
+		];
+		const history = createMemoryHistory();
+		const route = '/';
+		history.push(route);
+		const result = render(
+			<Router history={history}>
+				<Route>
+					<Tasks tasks={tasks} />
+				</Route>
+			</Router>
+		);
+
+		// assert
+		expect(result.container.getElementsByClassName('task').length).toBe(2);
+
+		tasks.push({
+			text: 'Fake Task #3',
+			date: '2021-06-01T20:30:00.000Z',
+			reminder: false,
+			id: 3,
+		});
+
+		result.rerender(
+			<Router history={history}>
+				<Route>
+					<Tasks tasks={tasks} />
+				</Route>
+			</Router>
+		);
 
 		// assert
 		expect(result.container.getElementsByClassName('task').length).toBe(3);
+	});
+
+	it('should only render 2 tasks when 1 is deleted', () => {
+		// arrange / act
+		const tasks = [
+			{
+				text: 'Fake Task #1',
+				date: '2021-06-01T20:30:00.000Z',
+				reminder: false,
+				id: 1,
+			},
+			{
+				text: 'Fake Task #2',
+				date: '2021-06-17T01:30:00.000Z',
+				reminder: false,
+				id: 2,
+			},
+		];
+		const history = createMemoryHistory();
+		const route = '/';
+		history.push(route);
+		const result = render(
+			<Router history={history}>
+				<Route>
+					<Tasks tasks={tasks} />
+				</Route>
+			</Router>
+		);
+
+		// assert
+		expect(result.container.getElementsByClassName('task').length).toBe(2);
+
+		tasks.pop();
+
+		result.rerender(
+			<Router history={history}>
+				<Route>
+					<Tasks tasks={tasks} />
+				</Route>
+			</Router>
+		);
+
+		// assert
+		expect(result.container.getElementsByClassName('task').length).toBe(1);
+	});
+
+	it('temp name', () => {
+		// arrange
+		const tasks = [
+			{
+				text: 'Fake Task #1',
+				date: '2021-06-01T20:30:00.000Z',
+				reminder: false,
+				id: 1,
+			},
+			{
+				text: 'Fake Task #2',
+				date: '2021-06-17T01:30:00.000Z',
+				reminder: false,
+				id: 2,
+			},
+		];
+		const history = createMemoryHistory();
+		const route = '/';
+
+		// act
+		history.push(route);
+		render(
+			<Router history={history}>
+				<Route path='/'>
+					<Tasks tasks={tasks} />
+				</Route>
+			</Router>
+		);
+
+		// assert
+		expect(screen.getByText('Fake Task #1')).toBeInTheDocument();
+		expect(screen.getByText('Fake Task #2')).toBeInTheDocument();
+		userEvent.click(screen.getByRole('button', {name: 'edit task 1'}));
+		expect(screen.queryByRole('button', {name: 'edit task 1'})).not.toBeInTheDocument();
+		expect(screen.getByText('Fake Task #2')).toBeInTheDocument();
 	});
 });
